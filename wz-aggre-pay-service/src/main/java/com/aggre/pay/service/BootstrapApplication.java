1c78e5d444c302c3b4b758ca651ed0e0c8f93b21
tianjunwei <924862077@qq.com>
2019-03-11 22:21:48 +0800

    添加工程

--- wz-aggre-pay-service/src/main/java/com/aggre/pay/service/BootstrapApplication.java
+++
@@ -1,9 +0,0 @@
-package com.aggre.pay.service;
-
-/**
- * @author tianjunwei
- * @date 2019/3/11 20:29
- */
-
-public class BootstrapApplication {
-}
---
+++ wz-aggre-pay-service/wz-aggre-pay-service-springcloud/wz-aggre-pay-service-springcloud-service/src/main/java/com/wz/aggre/pay/service/springcloud/service/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author tianjunwei
+ * 2019/3/11 22:20
+ */
+package com.wz.aggre.pay.service.springcloud.service;
