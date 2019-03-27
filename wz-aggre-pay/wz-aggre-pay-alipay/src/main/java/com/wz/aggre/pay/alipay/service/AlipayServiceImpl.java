96c6446bbc3cd3ffe5601371351656ff605be7cc
tianjunwei <924862077@qq.com>
2019-03-27 21:42:34 +0800

    添加alipay实体

--- wz-aggre-pay/wz-aggre-pay-alipay/src/main/java/com/wz/aggre/pay/alipay/service/AlipayServiceImpl.java
+++ wz-aggre-pay/wz-aggre-pay-alipay/src/main/java/com/wz/aggre/pay/alipay/service/AlipayServiceImpl.java
@@ -1,7 +1,10 @@
 package com.wz.aggre.pay.alipay.service;
 
+import com.alipay.api.AlipayApiException;
 import com.alipay.api.AlipayClient;
 import com.alipay.api.DefaultAlipayClient;
+import com.alipay.api.request.*;
+import com.alipay.api.response.*;
 import com.wz.aggre.pay.alipay.model.*;
 import com.wz.aggre.pay.alipay.model.app.AppPayModel;
 import com.wz.aggre.pay.alipay.model.pc.PcPayModel;
@@ -20,79 +23,188 @@ public class AlipayServiceImpl implements AlipayService {
     public String appPay(AppPayModel appPayModel) {
 
         AlipayClient alipayClient = getAlipayClient(appPayModel);
-
-        return null;
+        AlipayTradeAppPayRequest request = new AlipayTradeAppPayRequest();
+        request.setBizContent("");
+        try {
+            AlipayTradeAppPayResponse response = alipayClient.pageExecute(request);
+            if (response.isSuccess()) {
+               return response.getBody();
+            } else {
+                return response.getMsg();
+            }
+        }catch (AlipayApiException ex){
+            throw new IllegalStateException(ex);
+        }
     }
 
     @Override
     public String wapPay(WapPayModel wapPayModel) {
 
         AlipayClient alipayClient = getAlipayClient(wapPayModel);
-
-
-        return null;
+        AlipayTradeWapPayRequest request = new AlipayTradeWapPayRequest();
+        request.setBizContent("");
+        try {
+            AlipayTradeWapPayResponse  response = alipayClient.pageExecute(request);
+            if (response.isSuccess()) {
+                return response.getBody();
+            } else {
+                return response.getMsg();
+            }
+        }catch (AlipayApiException ex){
+            throw new IllegalStateException(ex);
+        }
     }
 
     @Override
     public String pcPay(PcPayModel pcPayModel) {
 
         AlipayClient alipayClient = getAlipayClient(pcPayModel);
+        AlipayTradePagePayRequest request = new AlipayTradePagePayRequest();
+        request.setBizContent("");
 
-        return null;
+        try {
+            AlipayTradePagePayResponse response = alipayClient.pageExecute(request);
+            if (response.isSuccess()) {
+                return response.getBody();
+            } else {
+                return response.getMsg();
+            }
+        }catch (AlipayApiException ex){
+            throw new IllegalStateException(ex);
+        }
     }
 
     @Override
     public String tradePrecreate(TradePrecreateModel tradePrecreateModel) {
 
         AlipayClient alipayClient = getAlipayClient(tradePrecreateModel);
+        AlipayTradePrecreateRequest request = new AlipayTradePrecreateRequest();
+        request.setBizContent("");
 
-        return null;
+        try {
+            AlipayTradePrecreateResponse response = alipayClient.execute(request);
+            if (response.isSuccess()) {
+                return response.getBody();
+            } else {
+                return response.getMsg();
+            }
+        }catch (AlipayApiException ex){
+            throw new IllegalStateException(ex);
+        }
     }
 
     @Override
     public String tradeCreate(TradeCreateModel tradeCreateModel) {
 
         AlipayClient alipayClient = getAlipayClient(tradeCreateModel);
-        return null;
+        AlipayTradeCreateRequest request = new AlipayTradeCreateRequest();
+        request.setBizContent("");
+        try {
+            AlipayTradeCreateResponse response = alipayClient.execute(request);
+            if (response.isSuccess()) {
+                return response.getBody();
+            } else {
+                return response.getMsg();
+            }
+        }catch (AlipayApiException ex){
+            throw new IllegalStateException(ex);
+        }
     }
 
     @Override
     public String tradePay(TradePayModel tradePayModel) {
 
         AlipayClient alipayClient = getAlipayClient(tradePayModel);
+        AlipayTradePayRequest request = new AlipayTradePayRequest();
+        request.setBizContent("");
 
-        return null;
+        try {
+            AlipayTradePayResponse response = alipayClient.execute(request);
+            if (response.isSuccess()) {
+                return response.getBody();
+            } else {
+                return response.getMsg();
+            }
+        }catch (AlipayApiException ex){
+            throw new IllegalStateException(ex);
+        }
     }
 
     @Override
     public String tradeClose(TradeCloseModel tradeCloseModel) {
 
         AlipayClient alipayClient = getAlipayClient(tradeCloseModel);
+        AlipayTradeCloseRequest request = new AlipayTradeCloseRequest();
+        request.setBizContent("");
 
-        return null;
+        try {
+            AlipayTradeCloseResponse response = alipayClient.execute(request);
+            if (response.isSuccess()) {
+                return response.getBody();
+            } else {
+                return response.getMsg();
+            }
+        }catch (AlipayApiException ex){
+            throw new IllegalStateException(ex);
+        }
     }
 
     @Override
     public String tradeRefund(TradeRefundModel tradeRefundModel) {
 
         AlipayClient alipayClient = getAlipayClient(tradeRefundModel);
+        AlipayTradeRefundRequest request = new AlipayTradeRefundRequest();
+        request.setBizContent("");
 
-        return null;
+
+        try {
+            AlipayTradeRefundResponse response = alipayClient.execute(request);
+            if (response.isSuccess()) {
+                return response.getBody();
+            } else {
+                return response.getMsg();
+            }
+        }catch (AlipayApiException ex){
+            throw new IllegalStateException(ex);
+        }
     }
 
     @Override
     public String tradeRefundQuery(RefundQueryModel refundQueryModel) {
 
         AlipayClient alipayClient = getAlipayClient(refundQueryModel);
-
-        return null;
+        AlipayTradeFastpayRefundQueryRequest request = new AlipayTradeFastpayRefundQueryRequest();
+        request.setBizContent("");
+        try {
+            AlipayTradeFastpayRefundQueryResponse response = alipayClient.execute(request);
+            if (response.isSuccess()) {
+                return response.getBody();
+            } else {
+                return response.getMsg();
+            }
+        }catch (AlipayApiException ex){
+            throw new IllegalStateException(ex);
+        }
     }
 
     @Override
     public String orderSettle(OrderSettleModel orderSettleModel) {
-        return null;
-    }
 
+        AlipayClient alipayClient = getAlipayClient(orderSettleModel);
+        AlipayTradeOrderSettleRequest request = new AlipayTradeOrderSettleRequest();
+        request.setBizContent("");
+
+        try {
+            AlipayTradeOrderSettleResponse response = alipayClient.execute(request);
+            if (response.isSuccess()) {
+                return response.getBody();
+            } else {
+                return response.getMsg();
+            }
+        }catch (AlipayApiException ex){
+            throw new IllegalStateException(ex);
+        }
+    }
 
     public AlipayClient getAlipayClient(BaseAlipayConfigModel baseAlipayConfig){
         AlipayClient alipayClient = new DefaultAlipayClient(baseAlipayConfig.getUrl(),baseAlipayConfig.getApp_id(),baseAlipayConfig.getRsa_private_key()
--- wz-aggre-pay/wz-aggre-pay-alipay/src/test/java/com/wz/aggre/pay/alipay/test/BootstrapApplication.java
+++
@@ -1,9 +0,0 @@
-package com.wz.aggre.pay.alipay.test;
-
-/**
- * @author tianjunwei
- * @date 2019/3/11 20:29
- */
-
-public class BootstrapApplication {
-}
